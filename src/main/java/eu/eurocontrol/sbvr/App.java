8c9670913bb93f858422129f3d64c63086949483
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2023-03-15 22:07:14 +0100

    WIP

---
+++ src/main/java/eu/eurocontrol/sbvr/App.java
@@ -0,0 +1,13 @@
+package eu.eurocontrol.sbvr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
