112ef49d12e973a5ec530d7bf603b9d638dea92c
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2023-03-22 00:35:18 +0100

    Cleanup

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ ANTLR4 getting started https://github.com/antlr/antlr4/blob/master/doc/getting-s
 mvn antlr4:help -Ddetail=true -Dgoal=antlr4
 mvn antlr4:antlr4
 mvn package
+
+# To Run:
 java -jar target/sbvr-parser-1.0-SNAPSHOT-jar-with-dependencies.jar App
 
 # To Test:
+mvn test
+
 cd target/classes
 grun eu.eurocontrol.sbvr.AixmSbvr statement -gui
 # type:
 # It is obligatory that at-least 3 abc
 # Ctrl-D
-grun eu.eurocontrol.sbvr.ArrayInit init -gui
-# type "{ init 3 }" and ctrl-d; or give it a filename
-
-
 ```
--- src/main/java/eu/eurocontrol/sbvr/App.java
+++ src/main/java/eu/eurocontrol/sbvr/App.java
@@ -2,6 +2,7 @@ package eu.eurocontrol.sbvr;
 
 import org.antlr.v4.runtime.*; 
 import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.CharStreams;
 
 /**
  * Hello world!
@@ -10,11 +11,11 @@ import org.antlr.v4.runtime.tree.*;
 public class App {
     public static void main(String[] args) throws Exception {
         // create a CharStream that reads from standard input
-        ANTLRInputStream input = new ANTLRInputStream(System.in); // create a lexer that feeds off of input CharStream
-        ArrayInitLexer lexer = new ArrayInitLexer(input); // create a buffer of tokens pulled from the lexer
+        CharStream input = CharStreams.fromStream(System.in); // create a lexer that feeds off of input CharStream
+        AixmSbvrLexer lexer = new AixmSbvrLexer(input); // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
-        ArrayInitParser parser = new ArrayInitParser(tokens);
-        ParseTree tree = parser.init(); // begin parsing at init rule
+        AixmSbvrParser parser = new AixmSbvrParser(tokens);
+        ParseTree tree = parser.statement(); // begin parsing at init rule
         System.out.println(tree.toStringTree(parser)); // print LISP-style tree }}
     }
 }
