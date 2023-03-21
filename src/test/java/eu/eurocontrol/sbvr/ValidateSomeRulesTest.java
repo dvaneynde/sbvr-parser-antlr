81852c0e276512b1860bad22d2999d4f055a244c
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2023-03-22 00:20:30 +0100

    Upgrade test.

--- src/test/java/eu/eurocontrol/sbvr/ValidateSomeRulesTest.java
+++ src/test/java/eu/eurocontrol/sbvr/ValidateSomeRulesTest.java
@@ -12,34 +12,11 @@ import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import org.junit.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+public class ValidateSomeRulesTest
+{
+    @Test
     public void testApp() {
         AixmSbvrLexer lexer = new AixmSbvrLexer(
                 CharStreams.fromString("It is obligatory that someField has not assigned ABC value"));
@@ -72,5 +49,4 @@ public class AppTest
                     offendingSymbol + ": " + msg);
         }
     }
-
 }
